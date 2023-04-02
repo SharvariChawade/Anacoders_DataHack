e762f5b708114783b264dfda976b1f3526b91287
Shyambhuva4 <91263012+Shyambhuva4@users.noreply.github.com>
2023-04-02 05:40:47 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,29 +8,27 @@ from scipy.sparse import csr_matrix
 from sklearn.neighbors import NearestNeighbors
 
 st.title("Food Recommendation System")
-st.text("Let us help you with ordering")
-st.image("foood.jpg")
+# col1, col2, col3 = st.columns(3)
+# with col1:
+#     st.write(' ')
 
-## nav = st.sidebar.radio("Navigation",["Home","IF Necessary 1","If Necessary 2"])
-
-st.subheader("Whats your preference?")
-vegn = st.radio("Vegetables or none!",["veg","non-veg"],index = 1) 
-
-st.subheader("What Cuisine do you prefer?")
-cuisine = st.selectbox("Choose your favourite!",['Healthy Food', 'Snack', 'Dessert', 'Japanese', 'Indian', 'French',
-       'Mexican', 'Italian', 'Chinese', 'Beverage', 'Thai'])
+# with col2:
+    
+st.text("Let us help you with food")
+st.image("food.jpg")
+# with col3:
+#     st.write(' ')
 
 
-st.subheader("How well do you want the dish to be?")  #RATING
-val = st.slider("from poor to the best!",0,10)
 
-food = pd.read_csv("food.csv")
-ratings = pd.read_csv("ratings.csv")
-rs=pd.read_csv('rs.csv')
-combined = pd.merge(ratings, food, on='Food_ID')
-#ans = food.loc[(food.C_Type == cuisine) & (food.Veg_Non == vegn),['Name','C_Type','Veg_Non']]
+st.subheader("How many recommendation you want?")  
+val = st.slider("from 1 to the 10!",1,10)
 
-# ans = combined.loc[(combined.C_Type == cuisine) & (combined.Veg_Non == vegn)& (combined.Rating >= val),['Name','C_Type','Veg_Non']]
+
+rs = pd.read_csv("rs.csv")
+
+
+
 names = rs['antecedents'].tolist()
 x = np.array(names)
 ans1 = np.unique(x)
@@ -42,29 +40,8 @@ if bruh == True:
 
 
 ##### IMPLEMENTING RECOMMENDER ######
-# dataset = ratings.pivot_table(index='Food_ID',columns='User_ID',values='Rating')
-# dataset.fillna(0,inplace=True)
-# csr_dataset = csr_matrix(dataset.values)
-# dataset.reset_index(inplace=True)
-
-# model = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=20, n_jobs=-1)
-# model.fit(csr_dataset)
 
 def food_recommendation(Food_Name):
-    # n = 10
-    # FoodList = food[food['Name'].str.contains(Food_Name)]  
-    # if len(FoodList):        
-    #     Foodi= FoodList.iloc[0]['Food_ID']
-    #     Foodi = dataset[dataset['Food_ID'] == Foodi].index[0]
-    #     distances , indices = model.kneighbors(csr_dataset[Foodi],n_neighbors=n+1)    
-    #     Food_indices = sorted(list(zip(indices.squeeze().tolist(),distances.squeeze().tolist())),key=lambda x: x[1])[:0:-1]
-    #     Recommendations = []
-    #     for val in Food_indices:
-    #         Foodi = dataset.iloc[val[0]]['Food_ID']
-    #         i = food[food['Food_ID'] == Foodi].index
-    #         Recommendations.append({'Name':food.iloc[i]['Name'].values[0],'Distance':val[1]})
-    #     df = pd.DataFrame(Recommendations,index=range(1,n+1))
-    #     return df['Name']
      recommend=[]  
 
      for i in range(len(rs)):
@@ -81,10 +58,7 @@ def food_recommendation(Food_Name):
 
 
 display = food_recommendation(finallist)
-#names1 = display['Name'].tolist()
 
-#x1 = np.array(names)
-#ans2 = np.unique(x1)
 if bruh == True:
     bruh1 = st.checkbox("We also Recommend : ")
     if bruh1 == True:
